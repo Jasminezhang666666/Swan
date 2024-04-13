c31bbc2d2fb2f9aa0a7e0448a8ecfce098336a1e
Jasminezhang666666 <jz5144@nyu.edu>
2024-04-13 16:50:12 +0800

    Lerping works

--- Assets/Scripts/FanInventory.cs
+++ Assets/Scripts/FanInventory.cs
@@ -6,10 +6,15 @@ public class FanInventoryManager : MonoBehaviour
     public List<Transform> slotPositions;  // References to empty GameObjects as slot positions for visible items
     public List<FanItem> inventorySlots;   // List of FanItem components, each attached to a UI slot
     [SerializeField] private Transform storagePosition;  // Transform for storing other items
+    [SerializeField] private float moveSpeed = 2.0f;  // Speed of the lerp movement
+
+    private bool isLerping = false;  // Check if currently lerping
+    private List<Vector3> targetPositions = new List<Vector3>();  // Target positions for lerping
 
     private void Start()
     {
-        UpdateItemPositions();  // Position items according to the empty GameObjects
+        SetupTargetPositions();  // Setup initial target positions
+        UpdateItemPositions();  // Initialize positions
     }
 
     private void Update()
@@ -22,33 +27,63 @@ public class FanInventoryManager : MonoBehaviour
         {
             RotateItems(1);   // Rotate right
         }
+
+        if (isLerping)
+        {
+            LerpItems();
+        }
     }
 
-    // Update the positions of each inventory item based on slot positions
-    private void UpdateItemPositions()
+    private void SetupTargetPositions()
     {
-        int itemCount = inventorySlots.Count;
-        int slotCount = slotPositions.Count;
-
+        targetPositions.Clear();
         for (int i = 0; i < inventorySlots.Count; i++)
         {
-            if (itemCount == 1 && i == 0)
+            if (i < slotPositions.Count)
             {
-                // Position the single item in the second slot (center slot)
-                inventorySlots[i].transform.position = slotPositions[Mathf.Clamp(1, 0, slotCount - 1)].position;
+                targetPositions.Add(slotPositions[i].position);
             }
-            else if (i < slotCount)
+            else
+            {
+                targetPositions.Add(storagePosition.position);
+            }
+        }
+        isLerping = true; // Enable lerping since we just set up new target positions
+    }
+
+    private void LerpItems()
+    {
+        bool stillLerping = false;
+        for (int i = 0; i < inventorySlots.Count; i++)
+        {
+            if (Vector3.Distance(inventorySlots[i].transform.position, targetPositions[i]) > 0.01f)
+            {
+                inventorySlots[i].transform.position = Vector3.Lerp(inventorySlots[i].transform.position, targetPositions[i], Time.deltaTime * moveSpeed);
+                stillLerping = true;
+            }
+            else
+            {
+                inventorySlots[i].transform.position = targetPositions[i];
+            }
+        }
+        isLerping = stillLerping;  // Only stop lerping when all items have reached their target positions
+    }
+
+    private void UpdateItemPositions()
+    {
+        for (int i = 0; i < inventorySlots.Count; i++)
+        {
+            if (i < slotPositions.Count)
             {
                 inventorySlots[i].transform.position = slotPositions[i].position;
             }
             else
             {
-                inventorySlots[i].transform.position = storagePosition.position; // Move excess items to the storage position
+                inventorySlots[i].transform.position = storagePosition.position;
             }
         }
     }
 
-    // Rotate the items in the inventory
     private void RotateItems(int direction)
     {
         if (inventorySlots.Count > 1)
@@ -65,7 +100,7 @@ public class FanInventoryManager : MonoBehaviour
                 inventorySlots.RemoveAt(0);
                 inventorySlots.Add(temp);
             }
-            UpdateItemPositions();  // Update positions after rotation
+            SetupTargetPositions();  // Update target positions after rotation
         }
     }
 
@@ -74,7 +109,7 @@ public class FanInventoryManager : MonoBehaviour
         if (!inventorySlots.Contains(item))
         {
             inventorySlots.Add(item);
-            UpdateItemPositions();  // Update positions to accommodate the new item
+            SetupTargetPositions();  // Update target positions
         }
     }
 
@@ -83,7 +118,7 @@ public class FanInventoryManager : MonoBehaviour
         if (inventorySlots.Contains(item))
         {
             inventorySlots.Remove(item);
-            UpdateItemPositions();  // Update positions after removing the item
+            SetupTargetPositions();  // Update target positions
         }
     }
 }

281b768ae393c353749d3d630210af7edfd9579a
Jasminezhang666666 <jz5144@nyu.edu>
2025-02-21 16:57:18 -0500

    Interview Background Music Insert (can't loop now)

--- Assets/FungusSound.cs
+++ Assets/FungusSound.cs
@@ -8,9 +8,15 @@ public class FungusSound : MonoBehaviour
     [Header("Introduction Sounds")]
     public AK.Wwise.Event Snd_NotesDown;
     public AK.Wwise.Event Snd_BrushItOff;
+    public AK.Wwise.Event Snd_IntroBackground;
 
 
     //Introduction
+    public void PlayBackground()
+    {
+        Snd_IntroBackground.Post(this.gameObject);
+    }
+
     public void PlayNotesDown()
     {
         Snd_NotesDown.Post(this.gameObject);

7ee6e5dcff9559e7979b6e050156270fdecfe8e1
YIMU LIN <yl9772@nyu.edu>
2025-03-13 17:41:03 -0400

    Wwise Update

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,22 +13,41 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_0_1_CHAPTER_TRANSITION = 3709892110U;
         static const AkUniqueID PLAY_0_2_PAPER_BURNING = 905968582U;
         static const AkUniqueID PLAY_0_4_PEN_WRITING = 2151942286U;
         static const AkUniqueID PLAY_0_6_CROSSING_OUT = 2704214433U;
         static const AkUniqueID PLAY_1_1_LOW_STRING_BACKGROUND = 2986374075U;
+        static const AkUniqueID PLAY_4_1_TRIPPING__WITH_BELONGINGS_ = 3057919114U;
         static const AkUniqueID PLAY_4_2_DOOR_OPEN_AND_CLOSE = 3653202828U;
+        static const AkUniqueID PLAY_5_1_HEAVY_DOOR_OPEN = 3335905725U;
+        static const AkUniqueID PLAY_5_2_COMMOTION__BUILD_UP_ = 2403330109U;
+        static const AkUniqueID PLAY_5_2_COMMOTION__LOOPING_ = 3003108111U;
+        static const AkUniqueID PLAY_5_3_BALLERINA_1 = 2224549712U;
+        static const AkUniqueID PLAY_5_4_BALLERINA_2 = 1115268924U;
+        static const AkUniqueID PLAY_5_5_BALLERINA_3 = 1293770752U;
+        static const AkUniqueID PLAY_5_8_CURTAIN = 502342557U;
+        static const AkUniqueID PLAY_5_9_COMMOTION__DISTORTED_ = 3919647464U;
+        static const AkUniqueID PLAY_5_10_COMMOTION__REVERB_ = 2722744558U;
+        static const AkUniqueID PLAY_6_1_DANCE_ROOM_CONVERSATION = 2771490912U;
+        static const AkUniqueID PLAY_6_2_WOODEN_CHAIR = 3239869686U;
+        static const AkUniqueID PLAY_6_3_THEATER_LIGHTS = 1253577752U;
+        static const AkUniqueID PLAY_9_1_DOORSLAM = 4126114025U;
+        static const AkUniqueID PLAY_9_1_RAIN_AND_THUNDER = 3596488037U;
+        static const AkUniqueID PLAY_10_1_COMMOTION__MIRROR_ = 1419623729U;
         static const AkUniqueID PLAY_ETUDE1 = 3673625560U;
         static const AkUniqueID PLAY_ETUDE_1_EASY = 50291956U;
         static const AkUniqueID PLAY_FOOTSTEPS_PRACTICE_ROOM = 3182324973U;
         static const AkUniqueID PLAY_INTERVIEW_LOOP_BGM = 3238762647U;
         static const AkUniqueID PLAY_INTRO_MUSIC = 1009565364U;
+        static const AkUniqueID PLAY_OPHELIA___112_BPM = 3133596185U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID MUSICMANAGER = 1618242883U;
+        static const AkUniqueID RHYTHMGAMES = 3062143902U;
     } // namespace BANKS
 
     namespace BUSSES
